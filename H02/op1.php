329afd501d81a317405f02a8e032df25ab78575c
FerdiFerdif <42802261+FerdiFerdif@users.noreply.github.com>
2019-01-08 10:07:11 +0100

    Git

---
+++ H02/op1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ferdi
+ * Date: 1/8/2019
+ * Time: 8:41 AM
+ */
+
+$a = "Ik heb niet gefaald. ";
+$b = "die niet werken. ";
+$c = 1000  ;
+
+echo $a . 'Ik heb alleen ' . $c . ' manieren gevonden ' . $b;
+
+$d = "die geen fout";
+$e = "en ";
+$f = "niet.";
+
+echo '<br> Mensen ' . $d . $e  . ' werken ' . $f;
+
+$g = "ry";
+$h = ", but";
+$j = "fail";
+
+echo '<br> T' . $g . $h . ' never fail to try.';
+
+$k = " \"omgaan ";
+$n = "met ";
+$m = "teleu";
+
+echo '<br> De cursus ' . $k . $n . $m . 'rstellingen" kan vanavond helaas niet doorgaan.';
+
+
